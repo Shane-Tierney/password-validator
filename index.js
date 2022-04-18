cb30e2bdf03ef57ee0dc596be3a285f11fee6f84
Shane Tierney <sptierney713@gmail.com>
2022-04-17 20:43:27 -0400

    number check and character check are now functioning properly

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const validatePassword = (password) => {
-  let length = lengthCheck(password)
-  let lower = lowerCaseCheck(password)
-  let upper = upperCaseCheck(password)
-  let numb = numberCheck(password)
-  let character = characterCheck(password)
-
-  if (length && lower && upper && numb && character) {
-    return true
-  } else return false
-}
-
 const lengthCheck = (password) => {
   if (password.length >= 8) {
     return true
@@ -17,43 +5,76 @@ const lengthCheck = (password) => {
 }
 
 const lowerCaseCheck = (password) => {
+  let value = false
+
   for (let i = 0; i < password.length; i++) {
     const letter = password[i]
 
-    if (letter.toUpperCase() !== letter) {
-      return true
+    if (letter.toLowerCase() === letter) {
+      value = true
     }
-
-    return false
   }
+
+  return value
 }
 
 const upperCaseCheck = (password) => {
+  let value = false
+
   for (let i = 0; i < password.length; i++) {
     const letter = password[i]
 
     if (letter.toUpperCase() === letter) {
-      return true
+      value = true
     }
-
-    return false
   }
+
+  return value
 }
 
 const numberCheck = (password) => {
+  let value = false
+
   for (let i = 0; i < password.length; i++) {
     const number = password[i]
 
-    if (Number(number)) {
-      return true
-    }
 
-    return false
+    if (Number(number)) {
+      value = true
+    }
   }
+
+  return value
 }
 
 const characterCheck = (password) => {
+  const specialCharacters = '!@#$%&*()\'+,-./:;<=>?[]^_`{|}'
+  let value = false
 
+  for (let i = 0; i < password.length; i++) {
+    const special = password[i]
+
+    if (specialCharacters.includes(special)) {
+      value = true
+    }
+  }
+
+  return value
 }
+
+const validatePassword = (password) => {
+  let length = lengthCheck(password)
+  let lower = lowerCaseCheck(password)
+  let upper = upperCaseCheck(password)
+  let numb = numberCheck(password)
+  let character = characterCheck(password)
+
+  console.log(length, lower, upper, numb, character)
+
+  if (length && lower && upper && numb && character) {
+    return true
+  } else return false
+}
+
 
 module.exports = validatePassword
